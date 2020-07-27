ccef4ce62c48c1a2966286beeeec8437f55817ed
Matt Martindale <martindalematt18@yahoo.com>
2020-07-26 18:16:11 -0600

    Setup launch screen

---
+++ AtomicAge/AtomicAge/Helper Files/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  AtomicAge
+//
+//  Created by Matthew Martindale on 7/26/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let tanBackground: UIColor = #colorLiteral(red: 1, green: 0.8862745098, blue: 0.7843137255, alpha: 1)
+}
